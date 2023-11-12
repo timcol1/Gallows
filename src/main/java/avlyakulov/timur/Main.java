f818b65b4b99bd339adfa69d90b0580ae66e27b1
Timur <avlyakulovtimur@gmail.com>
2023-11-12 11:44:44 +0200

    Initi repository

---
+++ src/main/java/avlyakulov/timur/Main.java
@@ -0,0 +1,8 @@
+package avlyakulov.timur;
+
+public class Main {
+    public static void main(String[] args) {
+        //заметки по реализации можно через message bundle в зависимости от локации человека
+        //выдавать разные языки то есть нужно передавать локацию человека.
+    }
+}
